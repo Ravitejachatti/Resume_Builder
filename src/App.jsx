a0370ce9c32878c5a92b5475999c38aec48f5256
raviteja chatti <ravitejachatti@ravitejas-MacBook-Air.local>
2025-03-13 22:25:54 +0530

    done with the second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResumeBuilder from './resumeBuilder';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello, Redux Demo!</h1>
+      <ResumeBuilder />
+    </>
+  );
+};
+
+export default App;
